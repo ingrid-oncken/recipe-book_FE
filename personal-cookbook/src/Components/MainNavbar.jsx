6e0d9ac7eae999aa5851b572dbc9f09af47f0b54
Ingrid Oncken <ingrid.oncken@gmail.com>
2022-02-10 21:13:23 +0000

    building

---
+++ personal-cookbook/src/Components/MainNavbar.jsx
@@ -0,0 +1,28 @@
+import RecipeModal from './RecipeModal'
+import logo from '../logo.svg'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { useState } from 'react'
+
+const MainNavbar = () => {
+  const [modalShow, setModalShow] = useState(false)
+
+  return (
+    <Navbar bg="warning" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto font-weight-bold">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link onClick={() => setModalShow(true)}>+ Recipe</Nav.Link>
+            <Nav.Link href="#Search">Search</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+      <RecipeModal show={modalShow} onHide={() => setModalShow(false)} />
+    </Navbar>
+  )
+}
+export default MainNavbar
