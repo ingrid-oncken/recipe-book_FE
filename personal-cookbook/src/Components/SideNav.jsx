81b645c01f8e85c3c9cfd8b2b294d994575160fa
Ingrid Oncken <ingrid.oncken@gmail.com>
2022-02-15 05:28:54 +0000

    SideNav accordion

--- personal-cookbook/src/Components/SideNav.jsx
+++ personal-cookbook/src/Components/SideNav.jsx
@@ -1,7 +1,15 @@
-import { Col, Form, FormControl, Nav } from 'react-bootstrap'
+import {
+  Accordion,
+  Button,
+  Card,
+  Col,
+  Form,
+  FormControl,
+  Nav,
+} from 'react-bootstrap'
 
 const SideNav = (props) => (
-  <Col sm={12} md={3} xl={2} className="sidenav-panel">
+  <Col sm={12} md={3} xl={2} className="sidenav-panel bg-light">
     <Form className="d-flex pt-3">
       <FormControl
         id="recipe-reach-input"
@@ -12,14 +20,63 @@ const SideNav = (props) => (
       />
     </Form>
     <hr className="text-dark" />
-    <Nav defaultActiveKey="/home" className="flex-column text-left">
-      <Nav.Link href="/home">Active</Nav.Link>
-      <Nav.Link eventKey="link-1">Link</Nav.Link>
-      <Nav.Link eventKey="link-2">Link</Nav.Link>
-      <Nav.Link eventKey="disabled" disabled>
-        Disabled
-      </Nav.Link>
-    </Nav>
+
+    <Accordion>
+      <Card className="border-0  text-left">
+        <Card.Header>
+          <Accordion.Toggle
+            as={Button}
+            variant="link"
+            eventKey="0"
+            className="pl-0 text-muted"
+          >
+            Brekfast
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+            <Nav defaultActiveKey="/home" className="flex-column text-left">
+              <Nav.Link href="/home" className="text-secondary">
+                Pancakes
+              </Nav.Link>
+              <Nav.Link eventKey="link-1" className="text-secondary">
+                Quick bread
+              </Nav.Link>
+              <Nav.Link eventKey="link-2" className="text-secondary">
+                Tapioca
+              </Nav.Link>
+            </Nav>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      <Card className="border-0  text-left">
+        <Card.Header>
+          <Accordion.Toggle
+            as={Button}
+            variant="link"
+            eventKey="1"
+            className="pl-0  text-muted"
+          >
+            Salads
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="1">
+          <Card.Body>
+            <Nav defaultActiveKey="/home" className="flex-column text-left">
+              <Nav.Link href="/home" className="text-secondary">
+                Classic Cesar
+              </Nav.Link>
+              <Nav.Link eventKey="link-1" className="text-secondary">
+                Waldorf
+              </Nav.Link>
+              <Nav.Link eventKey="link-2" className="text-secondary">
+                Mediterranean
+              </Nav.Link>
+            </Nav>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
   </Col>
 )
 
