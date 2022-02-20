41706b89def854d41d4b607e869a5e8772fa209e
Ingrid Oncken <ingrid.oncken@gmail.com>
2022-02-20 08:04:32 +0000

    Update RecipeModal.jsx

--- personal-cookbook/src/Components/RecipeModal.jsx
+++ personal-cookbook/src/Components/RecipeModal.jsx
@@ -1,17 +1,26 @@
 import {
   Button,
+  ButtonToolbar,
   Col,
   Container,
   Dropdown,
   DropdownButton,
+  Form,
   FormControl,
   InputGroup,
+  ListGroup,
   Modal,
-  Nav,
   Row,
+  Tab,
 } from 'react-bootstrap'
-import { FaUserEdit, FaRegClock, FaTags } from 'react-icons/fa'
-import { GiForkKnifeSpoon } from 'react-icons/gi'
+import {
+  FaListOl,
+  FaUserEdit,
+  FaRegClock,
+  FaTags,
+  FaTrashAlt,
+} from 'react-icons/fa'
+import { GiCookingPot, GiForkKnifeSpoon, GiSecretBook } from 'react-icons/gi'
 import { RiKnifeLine } from 'react-icons/ri'
 
 const RecipeModal = (props) => {
@@ -80,10 +89,16 @@ const RecipeModal = (props) => {
                     <GiForkKnifeSpoon />
                   </InputGroup.Text>
                 </InputGroup.Prepend>
-                <FormControl
-                  aria-label="number of portions"
-                  placeholder="Portions"
-                />
+
+                <Form.Control as="select">
+                  <option>Portions</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                </Form.Control>
               </InputGroup>
             </Col>
             {/* End of portions */}
@@ -125,40 +140,247 @@ const RecipeModal = (props) => {
             </Col>
             {/* End of total time */}
           </Row>
-          <Row>
-            {/* Start tags nav */}
-            <Col xs={12}>
-              <Nav
-                className="justify-content-center"
-                variant="pills"
-                defaultActiveKey="/home"
-              >
-                <Nav.Item>
-                  <Nav.Link href="/home">Active</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="disabled" disabled>
-                    Disabled
-                  </Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Col>
-            {/* End of tags nav */}
-          </Row>
 
-          <InputGroup>
+          {/* Start tags Tab */}
+          <Tab.Container id="list-group-tabs-example">
+            <Row>
+              <Col sm={12}>
+                <ListGroup horizontal>
+                  <ListGroup.Item action variant="warning" href="#link1">
+                    Cooking Method
+                  </ListGroup.Item>
+                  <ListGroup.Item action variant="warning" href="#link2">
+                    Tags
+                  </ListGroup.Item>
+                </ListGroup>
+              </Col>
+              <Col sm={12}>
+                <Tab.Content>
+                  <Tab.Pane eventKey="#link1">
+                    <Form>
+                      <div className="mx-3 mt-2">
+                        <Form.Group controlId="formBasicCheckbox">
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Air-fry"
+                            id="Air-fry"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="BBQ"
+                            id="BBQ"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Deep-fry"
+                            id="Deep-fry"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Microwave"
+                            id="Microwave"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Multicooker"
+                            id="Multicooker"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Oven"
+                            id="Oven"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Robot"
+                            id="Robot"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Sous vide"
+                            id="Sous vide"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Stove"
+                            id="Stove"
+                            name="formHorizontalRadios"
+                          />
+                          <Form.Check
+                            inline
+                            type="radio"
+                            label="Vapor"
+                            id="Vapor"
+                            name="formHorizontalRadios"
+                          />
+                        </Form.Group>
+                      </div>
+                    </Form>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="#link2">
+                    <Form>
+                      <div className="mx-3 mt-2">
+                        <Form.Group controlId="formBasicCheckbox">
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Appetizer"
+                            id="Appetizer"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Fish"
+                            id="Fish"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Fit"
+                            id="Fit"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Gluten-free"
+                            id="Gluten-free"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Halal"
+                            id="Halal"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Kosher"
+                            id="Kosher"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Meat"
+                            id="Meat"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Poultry"
+                            id="Poultry"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Seafood"
+                            id="Seafood"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Tapas"
+                            id="Tapas"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Vegan"
+                            id="Vegan"
+                            name="formHorizontalChekboxes"
+                          />
+                          <Form.Check
+                            inline
+                            type="checkbox"
+                            label="Ovolacto-vegetarian"
+                            id="Ovolacto-vegetarian"
+                            name="formHorizontalChekboxes"
+                          />
+                        </Form.Group>
+                      </div>
+                    </Form>
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
+          {/* End of tags Tab */}
+
+          <InputGroup className="my-3">
             <InputGroup.Prepend>
-              <InputGroup.Text>With textarea</InputGroup.Text>
+              <InputGroup.Text>
+                <FaListOl />
+              </InputGroup.Text>
             </InputGroup.Prepend>
-            <FormControl as="textarea" aria-label="With textarea" />
+            <FormControl
+              as="textarea"
+              aria-label="With textarea"
+              placeholder="Ingredients"
+            />
           </InputGroup>
+          <InputGroup className="my-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                <GiCookingPot />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              as="textarea"
+              aria-label="With textarea"
+              placeholder="Preparation steps..."
+            />
+          </InputGroup>
+          <InputGroup className="my-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                <GiSecretBook />
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="With textarea"
+              placeholder="Personal notes"
+            />
+          </InputGroup>
+          <Form>
+            <Form.Group>
+              <Form.File id="exampleFormControlFile1" />
+            </Form.Group>
+          </Form>
         </Container>
       </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
+      <Modal.Footer className="px-4">
+        <Button variant="outline-danger" className="mr-auto">
+          <FaTrashAlt />
+        </Button>
+        <Button onClick={props.onHide} variant="warning">
+          Save
+        </Button>
       </Modal.Footer>
     </Modal>
   )
