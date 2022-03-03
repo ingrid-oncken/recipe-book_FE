f59a6485b616585e01c16e4da6a961450e4ac3e8
Ingrid Oncken <ingrid.oncken@gmail.com>
2022-03-03 09:27:20 +0000

    Update App.js

--- personal-cookbook/src/App.js
+++ personal-cookbook/src/App.js
@@ -20,7 +20,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
         </Routes>
-
         <Footer />
       </div>
     </BrowserRouter>
