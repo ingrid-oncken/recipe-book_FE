79e7806f4a24f4f9aac574958f91e6dfa689c60b
Ingrid Oncken <ingrid.oncken@gmail.com>
2022-03-18 22:51:17 +0000

    still troubleshooting the fetching

--- personal-cookbook/src/redux/actions/index.js
+++ personal-cookbook/src/redux/actions/index.js
@@ -21,14 +21,29 @@ export const registerAction = (userObject) => {
 
       let resp = await fetch('http://localhost:3001/users/register', {
         method: 'POST',
+        body: JSON.stringify(userObject),
+        headers: {
+          contentType: 'application/json',
+        },
       })
+
+      console.log(
+        `this is CLG of resp await fetch -- line 30 -- actions/index.js ${resp}`
+      )
+
       if (resp.ok) {
-        console.log(resp)
-        let user = await resp.json()
+        console.log(
+          `this is CLG of resp inside if -- line 34 -- actions/index.js ${resp}`
+        )
+
+        let user = resp.json()
+        console.log(`this is CLG USER -- line 40 -- actions/index.js ${user}`)
+
         dispatch({
           type: REGISTER_SUCCESS,
           payload: user,
         })
+
         dispatch({
           type: LOADING,
           payload: false,
@@ -39,7 +54,7 @@ export const registerAction = (userObject) => {
           type: ERROR,
           payload: resp.status,
         })
-        console.log('error inside register action ELSE')
+        console.log('error inside register action ELSE', resp.status)
       }
     } catch (error) {
       console.log(error)
