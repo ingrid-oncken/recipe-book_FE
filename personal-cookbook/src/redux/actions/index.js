33dd5cd51b9fe8130a652509bebf31ef3eafb3df
Ingrid Oncken <ingrid.oncken@gmail.com>
2022-03-19 00:11:40 +0000

    Update index.js

--- personal-cookbook/src/redux/actions/index.js
+++ personal-cookbook/src/redux/actions/index.js
@@ -8,7 +8,14 @@ import {
 } from './types.js'
 
 export const registerAction = (userObject) => {
+  console.log(
+    `this is CLG of userObject -- line 12 -- actions/index.js ${userObject}`
+  )
   return async (dispatch) => {
+    console.log(
+      `this is CLG of userObject -- line 16
+       -- actions/index.js ${userObject}`
+    )
     try {
       dispatch({
         type: LOADING,
@@ -30,14 +37,17 @@ export const registerAction = (userObject) => {
       console.log(
         `this is CLG of resp await fetch -- line 30 -- actions/index.js ${resp}`
       )
+      console.log(
+        `this is CLG of userObject -- line 34 -- actions/index.js ${userObject}`
+      )
 
       if (resp.ok) {
         console.log(
-          `this is CLG of resp inside if -- line 34 -- actions/index.js ${resp}`
+          `this is CLG of resp inside if -- line 39 -- actions/index.js ${resp}`
         )
 
         let user = resp.json()
-        console.log(`this is CLG USER -- line 40 -- actions/index.js ${user}`)
+        console.log(`this is CLG USER -- line 43 -- actions/index.js ${user}`)
 
         dispatch({
           type: REGISTER_SUCCESS,
