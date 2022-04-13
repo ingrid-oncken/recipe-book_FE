465f09821b325a5476e60eb3843703680516f529
Ingrid Oncken <ingrid.oncken@gmail.com>
2022-04-13 12:19:14 +0100

    get

--- personal-cookbook/src/Components/Home.jsx
+++ personal-cookbook/src/Components/Home.jsx
@@ -1,14 +1,60 @@
-import { CardDeck, Col } from 'react-bootstrap'
-
+import { Row, Col } from 'react-bootstrap'
 import RecipeCard from './RecipeCard'
+import { useState, useEffect } from 'react'
+import RecipeCards from './RecipeCard'
 
 const Home = () => {
+  const [recipe, setRecipe] = useState([])
+
+  const fetchRecipes = async (e) => {
+    console.log('fetch recipes before try')
+    try {
+      let res = await fetch(
+        `http://localhost:3001/recipes/62458c33f9aee5b9918c66c7`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization:
+              `${process.env.REACT_TOKEN_USER}`,
+          },
+        }
+      )
+
+      //console.log('clg RES -->', res)
+      // console.log('RECIPEEEESSSS!!!', recipe[2].recipeTitle)
+
+      const output = await res.json()
+      //console.log('OUTPUT', output[2].recipeTitle)
+
+      if (res.ok) {
+        setRecipe(output)
+
+        console.log('CLG RES IF RES.OK', res)
+      } else {
+        console.log('ELSE === !res.ok')
+        // console.log('CLG RES ELS !RES.OK')
+      }
+    } catch (error) {
+      console.log('Catch error of fetching Recipes --', error)
+    }
+  }
+
+  // const oneRecipe = recipe[2]
+  // console.log('recipe!!!', oneRecipe)
+
+  useEffect(() => {
+    fetchRecipes()
+  }, [])
+
   return (
-    <Col sm={12} md={9} xl={8}>
-      <CardDeck className="mb-3 mt-5">
-        <RecipeCard />
-      </CardDeck>
-    </Col>
+    <Row>
+      {recipe.map((r) => (
+        <Col xs={12} md={9} xl={8} key={r._id}>
+          <RecipeCards data={r} />
+        </Col>
+      ))}
+    </Row>
   )
 }
 
--- personal-cookbook/src/Components/RecipeCard.jsx
+++ personal-cookbook/src/Components/RecipeCard.jsx
@@ -1,63 +1,27 @@
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
-const RecipeCards = () => {
-  
-  const fetchRecipes = async (e) => {
-
-    console.log('fetch recipes before try')
-    try {
-      let res = await fetch(
-        
-
-        `http://localhost:3001/recipes/62458c33f9aee5b9918c66c7`,
-        {
-          method: 'GET',
-          body: JSON.stringify(newRecipe),
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ',
-          },
-        }
-      )
-
-      console.log('clg RES of newRecipe from the fetch of RecipeModal -->', res)
-      console.log('newRecipe.authorName', newRecipe.authorName)
-
-      const output = await res.json()
-      console.log('OUTPUT', output)
-
-      if (res.ok) {
-        setNewRecipe(output)
-        console.log('CLG RES IF RES.OK', res)
-      } else {
-        console.log('ELSE === !res.ok')
-        // TODO: display a green line "sth went wrong and couldn't save the recipe"
-      }
-    } catch (error) {
-      console.log('Catch error of fetching a newRecipe --', error)
-    }
-  }
-  
+const RecipeCards = ({ data }) => {
   return (
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Recipe title</Card.Title>
-      <Card.Subtitle className="mb-2 text-muted">
-        Cathegory SHows Here
-      </Card.Subtitle>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-      <Link className="card-link" to="/fullrecipe">
-        See full recipe...
-      </Link>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last opened 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-)}
+    <>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>{data.recipeTitle}</Card.Title>
+          <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+          <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.
+          </Card.Text>
+          <Link className="card-link" to="/fullrecipe">
+            See full recipe...
+          </Link>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last opened 3 mins ago</small>
+        </Card.Footer>
+      </Card>
+    </>
+  )
+}
 export default RecipeCards
